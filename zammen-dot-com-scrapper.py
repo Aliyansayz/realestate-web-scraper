9529d3d6fa932014000455f942740b6dc6867a35
Aliyansayz <56589240+Aliyansayz@users.noreply.github.com>
2023-04-13 19:34:09 +0500

    Update and rename .py to zammen-dot-com-scrapper.py

--- zammen-dot-com-scrapper.py
+++ zammen-dot-com-scrapper.py
@@ -3,6 +3,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
+"""
+HouseScraper = ZameenScrape('https://www.zameen.com/Homes/Lahore-1-1.html' , 5)
+HouseScraper.RunScrape()
+
+area_urls = ['https://www.zameen.com/Homes/Lahore-1-1.html', 'https://www.zameen.com/Homes/Lahore_Bahria_Town-509-1.html'
+             'https://www.zameen.com/Homes/Lahore_Johar_Town-93-1.html', 'https://www.zameen.com/Homes/Lahore_Defence_(DHA)_Phase_3-1454-1.html']
+LahoreHouseScraper = ZameenScrape(area_urls , 50)
+
+LahoreHouseScraper.RunScrapeList()
+"""
 class ZameenScrape:
   def __init__(self, url, pages_num ):
       self.url = url
@@ -10,11 +20,17 @@ class ZameenScrape:
 
   def RequestPage(self, current_page , url):
     url = url.split('1.html')
+    z = url[0].split('/')
+    area_name = z[4].split('-1')
+    property_type =z[3]
+    area_name = area_name[0].split('-')
+    area_name = area_name[0]
+    area_name , property_type
     url = url[0]+f'{current_page}.html'
 
     page = requests.get(url)
     soup = BeautifulSoup(page.content , 'html.parser')
-    return soup 
+    return soup , area_name , property_type
 
   def SelectData(self, soup):
     all_titles  = soup.find_all('a', class_='_7ac32433' )
@@ -23,6 +39,7 @@ class ZameenScrape:
     return all_titles ,all_area , all_price
 
   def StoreData(self, all_titles ,all_area , all_price , current_page , title_list, area_list, price_list  ): 
+    
     
     for name in all_titles:
       title_list.append(name.get('title'))
@@ -35,9 +52,9 @@ class ZameenScrape:
       
     return title_list ,  area_list , price_list
 
-  def SaveCsv(self, title_list ,  area_list , price_list):
+  def SaveCsv(self, title_list ,  area_list , price_list , area_name , property_type):
     df = pd.DataFrame({ "Name" : pd.Series(title_list) , "Area" : pd.Series(area_list) , "Price" : pd.Series(price_list) })
-    df.to_csv(f'Propertydata'  , index=False)
+    df.to_csv(f'{area_name}_{property_type}'  , index=False)
 
 
   def DefineScrape(self, url, pages_num  ):
@@ -46,12 +63,16 @@ class ZameenScrape:
     price_list = []
     for i in range(1,pages_num+1):     
       current_page = i 
-      soup = self.RequestPage(current_page , url)
+      soup , area_name , property_type = self.RequestPage(current_page , url)
       all_titles ,all_area , all_price = self.SelectData(soup)
       title_list ,  area_list , price_list = self.StoreData(all_titles ,all_area , all_price , current_page , title_list, area_list, price_list  )
       
-    self.SaveCsv(title_list ,  area_list , price_list)
+    self.SaveCsv(title_list ,  area_list , price_list , area_name , property_type)
 
   def RunScrape(self):
      self.DefineScrape(self.url, self.pages_num)
       
+  def RunScrapeList(self):
+     for value in self.url:      
+       self.DefineScrape(value, self.pages_num)
+
